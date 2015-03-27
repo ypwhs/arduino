85c71a317ad4e5bed4190a4d425cebd100f50370
ypwhs <915505626@qq.com>
2015-03-27 21:27:57 +0800

    master

--- _acar/_acar.ino
+++ _acar/_acar.ino
@@ -1,71 +1,71 @@
-#include <Servo.h> 
+#include <Servo.h>
 Servo myservo;
 int offset = 45;
 void setup() {
-  pinMode(5,OUTPUT);pinMode(6,OUTPUT);
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
+  pinMode(5, OUTPUT); pinMode(6, OUTPUT);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
   myservo.attach(11);
   myservo.write(45);
   Serial.begin(115200);
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
 }
 long s;
-void jishu(){
-  if(PIND&0X04){
+void jishu() {
+  if (PIND & 0X04) {
     s++;
   }
   else {
-  s--;
+    s--;
   }
 }
 
 long lasttime = millis();
 long last = 0;
 
-void setdir(int indir){
-  myservo.write(offset+indir);
+void setdir(int indir) {
+  myservo.write(offset + indir);
 }
-void setspeed(int inspd){
-  if(inspd>0){
-    digitalWrite(5,LOW);
-    analogWrite(6,inspd);
-  }else if(inspd<0){
-    analogWrite(5,inspd);
-    digitalWrite(6,LOW);
-  }else{
-    digitalWrite(5,LOW);
-    digitalWrite(6,LOW);
+void setspeed(int inspd) {
+  if (inspd > 0) {
+    digitalWrite(5, LOW);
+    analogWrite(6, inspd);
+  } else if (inspd < 0) {
+    analogWrite(5, inspd);
+    digitalWrite(6, LOW);
+  } else {
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
   }
 }
 
 void loop() {
   long nowtime = millis();
-  if(nowtime-lasttime>10){
-    long delta = s-last;
+  if (nowtime - lasttime > 10) {
+    long delta = s - last;
     Serial.println(delta);
-    last=s;
-    lasttime=nowtime;
+    last = s;
+    lasttime = nowtime;
   }
-  
-  if(Serial.available())
+
+  if (Serial.available())
   {
     char bufchar = Serial.read();
-    if(bufchar == 'w'){
+    if (bufchar == 'w') {
       setdir(0);
       setspeed(50);
     }
-    else if(bufchar == 's'){
+    else if (bufchar == 's') {
       setdir(0);
       setspeed(-50);
     }
-    else if(bufchar == 'a'){
+    else if (bufchar == 'a') {
       setdir(-30);
     }
-    else if(bufchar == 'd'){
+    else if (bufchar == 'd') {
       setdir(30);
-    }else if(bufchar == 'x'){
+    } else if (bufchar == 'x') {
       setspeed(0);
     }
   }
