15f44b92162136c11194f699a7a484432642c032
ypw <915505626@qq.com>
2015-04-28 20:41:19 +0800

    master
    smarthome

--- smarthome/smarthome.ino
+++ smarthome/smarthome.ino
@@ -3,21 +3,22 @@ void setup() {
   int i;
   for (i = 4; i < 8; i++) {
     pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
+    digitalWrite(i, HIGH);
   }
   Serial.begin(115200);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available()) {
+  if (Serial.available()) {
     char a = Serial.read();
-    a -= '1';
+    Serial.print(a);
+    a -= '0';
     if (a < 4) {
       digitalWrite(a + 3, !digitalRead(a + 3));
     } else if (a == 4) {
       int i;
-      for (i = 4; i < 8; i++) {
-        digitalWrite(i, LOW);
+      for (i = 4; i < 7; i++) {
+        digitalWrite(i, HIGH);
       }
     }
   }
