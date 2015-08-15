096fe10fdcdacf7e11850ed70948c4b263b71e92
ypw <915505626@qq.com>
2015-08-15 16:44:37 +0800

    1

--- doumao/doumao.ino
+++ doumao/doumao.ino
@@ -3,12 +3,10 @@ Servo myservo;
 void setup() 
 { 
   myservo.attach(9);
+  randomSeed(analogRead(A0));
 }
-  int j=120,f=20,de=800;
 void loop() 
-{ 
-  myservo.write(j+analogRead(A0)%f);
-  delay(de/2+analogRead(A0)%(de/2));
-  myservo.write(j-analogRead(A0)%f);
-  delay(de/2+analogRead(A0)%(de/2));
-} 
+{
+  myservo.write(random(180));
+  delay(random(5000));
+}
