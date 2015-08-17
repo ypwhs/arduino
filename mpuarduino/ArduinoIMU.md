69a6e11a21344fe9a6f1135901139fe571368ccb
ypw <915505626@qq.com>
2015-08-17 23:30:55 +0800

    1

---
+++ mpuarduino/ArduinoIMU.md
@@ -0,0 +1,4 @@
+# ArduinoIMU
+
+![](p.jpg)
+
--- mpuarduino/mpuarduino.ino
+++ mpuarduino/mpuarduino.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <Kalman.h>
-#include <TimerOne.h>
 
 #define    MPU9250_ADDRESS            0x68
 #define    MAG_ADDRESS                0x0C
@@ -29,7 +28,6 @@ void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
     Data[index++] = Wire.read();
 }
 
-
 // Write a byte (Data) in device (Address) at register (Register)
 void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
 {
@@ -40,11 +38,6 @@ void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
   Wire.endTransmission();
 }
 
-// Initial time
-long int ti;
-volatile bool intFlag = false;
-
-// Initializations
 void setup()
 {
   // Arduino initializations
@@ -69,51 +62,13 @@ void setup()
 
   //I2CwriteByte(MPU9250_ADDRESS, 106, 1 << 6);
   
-  pinMode(13, OUTPUT);
-  Timer1.initialize(10000);         // initialize timer1, and set a 1/2 second period
-  Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
-
-
-  // Store initial time
-  ti = millis();
 }
+unsigned long lasttime = 0;
+Kalman kalmanRoll;
+Kalman kalmanPitch;
 
-
-// Counter
-long int cpt = 0;
-
-void callback()
-{
-  intFlag = true;
-  digitalWrite(13, digitalRead(13) ^ 1);
-}
-
-
-unsigned long nLastTime = 0; //上一次读数的时间
-float fLastRoll = 0.0f; //上一次滤波得到的Roll角
-float fLastPitch = 0.0f; //上一次滤波得到的Pitch角
-Kalman kalmanRoll; //Roll角滤波器
-Kalman kalmanPitch; //Pitch角滤波器
-
-// Main loop, read and display data
 void loop()
 {
-  while (!intFlag);
-  intFlag = false;
-
-  // Display time
-//  Serial.print (millis() - ti, DEC);
-//  Serial.print ("\t");
-
-
-  // _______________
-  // ::: Counter :::
-
-  // Display data counter
-  //  Serial.print (cpt++,DEC);
-  //  Serial.print ("\t");
-
-
 
   // ____________________________________
   // :::  accelerometer and gyroscope :::
@@ -135,31 +90,6 @@ void loop()
   int16_t gz = Buf[12] << 8 | Buf[13];
 
 
-  // Display values
-/*
-  // Accelerometer
-  Serial.print (ax, DEC);
-  Serial.print ("\t");
-  Serial.print (ay, DEC);
-  Serial.print ("\t");
-  Serial.print (az, DEC);
-  Serial.print ("\t");
-
-  // Gyroscope
-  Serial.print (gx, DEC);
-  Serial.print ("\t");
-  Serial.print (gy, DEC);
-  Serial.print ("\t");
-  Serial.print (gz, DEC);
-  Serial.print ("\t");
-*/
-
-  // _____________________
-  // :::  Magnetometer :::
-
-
-  // Read register Status 1 and wait for the DRDY: Data Ready
-
   uint8_t ST1;
   do
   {
@@ -179,16 +109,6 @@ void loop()
   int16_t my = -(Mag[1] << 8 | Mag[0]);
   int16_t mz = -(Mag[5] << 8 | Mag[4]);
 
-/*
-  // Magnetometer
-  Serial.print (mx, DEC);
-  Serial.print ("\t");
-  Serial.print (my, DEC);
-  Serial.print ("\t");
-  Serial.print (mz, DEC);
-  Serial.print ("\t");
-*/
-
   //calculate
   float accX = ax * 4.0 / 32768 ;
   float accY = ay * 4.0 / 32768;
@@ -202,22 +122,21 @@ void loop()
   float yz = accZ * accZ + accY * accY;
   float xz = accX * accX + accZ * accZ;
   
-  float roll = acos(sqrt(xz)/accG);
-  float pitch = acos(sqrt(yz)/accG);
+  float roll = acos(sqrt(xz)/accG) * RAD_TO_DEG;
+  float pitch = acos(sqrt(yz)/accG) * RAD_TO_DEG;
   if(ay>0)roll=-roll;
   if(ax<0)pitch=-pitch;
   
-  //计算两次测量的时间间隔dt，以秒为单位
-  unsigned long nCurTime = micros();
-  float dt = (double)(nCurTime - nLastTime) / 1000000.0;
-  //对Roll角和Pitch角进行卡尔曼滤波
-  float fNewRoll = kalmanRoll.getAngle(roll, gyroX, dt);
-  float fNewPitch = kalmanPitch.getAngle(pitch, gyroY, dt);
+  float dt = (double)(micros() - lasttime) / 1000000.0;
 
-  Serial.print(fNewRoll*1000);
+  float roll2 = kalmanRoll.getAngle(roll, -gyroX, dt);
+  float pitch2 = kalmanPitch.getAngle(pitch, -gyroX, dt);
+
+  Serial.print(roll2);
   Serial.print("\t");
-  Serial.print(fNewPitch*1000);
+  Serial.print(pitch2);
   Serial.print("\t");
+  
   // End of line
   Serial.println("");
   //  delay(100);
