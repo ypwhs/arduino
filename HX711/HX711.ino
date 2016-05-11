aa3316dae77a9136e4c6010a406e4876439dfa4a
ypwhs <915505626@qq.com>
2016-05-11 18:36:25 +0800

    Update HX711.ino

--- HX711/HX711.ino
+++ HX711/HX711.ino
@@ -1,15 +1,9 @@
 #include <HX711.h>
-HX711 hx(4, 5);
-//4 SCK    5 DT
+HX711 hx(4, 5); //4 SCK    5 DT
 void setup() {
-  pinMode(13,OUTPUT);
-  pinMode(3,OUTPUT);
-  digitalWrite(3,HIGH);
   Serial.begin(9600);
 }
 
-void loop()
-{
-  double sum = 0;
+void loop() {
   Serial.println(abs(hx.read()));
 }
